ec6941768ed83520f6dd7eedf28e0b48532f31c0
Trashboat12 <yashpatel3242@gmail.com>
2026-04-15 02:35:13 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,15 @@ def create_app():
 
     app.secret_key = "dev_key"
 
+    from app import db
+    db.init_app(app)
+    
     from app.controllers.meals import meals_bp
     from app.controllers.foods import foods_bp
+    from app.controllers.analytics import analytics_bp
 
     app.register_blueprint(meals_bp)
     app.register_blueprint(foods_bp)
+    app.register_blueprint(analytics_bp)
 
     return app
